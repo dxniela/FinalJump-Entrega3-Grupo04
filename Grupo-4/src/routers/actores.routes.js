fba5bb5d543b98e70e3f4ef1b6824295c363ef34
Daniela <randrade.daniela@gmail.com>
2024-08-07 01:22:06 -0400

    fix: adding screenshots to readme

--- Grupo-4/src/routers/actores.routes.js
+++ Grupo-4/src/routers/actores.routes.js
@@ -3,14 +3,19 @@ module.exports = app => {
 
     const router = require("express").Router();
 
+    // Ruta para obtener todos los actores
     router.get("/", actorController.findAll);
 
-    router.get("/actores/:id", actorController.findOne);
+    // Ruta para obtener un actor por su id
+    router.get("/:id", actorController.findOne);
 
+    // Ruta para crear un actor
     router.post("/", actorController.create);
 
+    // Ruta para eliminar un actor por su id
     router.delete("/:id", actorController.destroy);
 
+    // Ruta para actualizar un actor por su id
     router.put("/:id", actorController.update);
 
     app.use("/api/actores", router);
